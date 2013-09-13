d6b9e1cf80d276f79780d23c0b62f23ec19df1f9
arita1000 <hoangminhminh@gmail.com>
2013-09-13 14:42:16 -0400

    Create ngo-bozdo-hw3.rkt

---
+++ ngo-bozdo-hw3.rkt
@@ -0,0 +1,29 @@
+;; Students: Hoang Ngo
+;; Username: hmngo
+;; ------------------------------
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Problem 1: A storm can be any one of the following:
+;;
+;;- hurricane: Information required for a hurricane consists of the name of the hurricane, 
+;;             the category (a number between 1 and 5, inclusive), the maximum sustained winds in miles per hour, 
+;;             the velocity of the storm in miles per hour, and the storm's heading (for example, NNW).
+;;- thunderstorm: The definition of a thunderstorm consists of the number of inches of rainfall, 
+;;                the maximum wind gust in miles per hour, the velocity of the storm in miles per hour, and its heading.
+;;- fire: A fire is represented by the number of square miles it covers, the number of days it has been raging, 
+;;        and the number of people displaced by the fire.
+;;        
+;;Develop a data definition for each type of storm described above, and a data definition for an itemization for storms. 
+;;Provide at least one example for each kind of storm.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define-struct hurricane (name category max-wind storm-velocity storm-destination))
+;; A hurricane is (make-hurricane String Natural Number Number String)
+;; interp. (make-hurricane name category max-wind storm-velocity storm-destination) is a hurricane with
+;;         name as its name
+;;         category as its category (1 to 5, inclusive)
+;;         max-wind as its maximum winds in miles per hour
+;;         storm-velocity as the storm's velocity
+;;         storm-destination as the storm's heading
+
+(define-struct thunderstorm)
