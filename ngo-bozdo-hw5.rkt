6a647c45c1aaab91e22ded0219d65bd009bed316
arita1000 <hoangminhminh@gmail.com>
2013-09-26 22:52:39 -0400

    problem 1, 3, 6

--- ngo-bozdo-hw5.rkt
+++ ngo-bozdo-hw5.rkt
@@ -6,3 +6,85 @@
 ;; Problem 1: Write the data definition(s) needed for this binary search tree.
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+; Data definition for book
+
+(define-struct book (title authors cost n-sold isbn book-list))
+;; a book is (make-book String ListOfAuthor Number Number Number ListOfBook)
+;; interp. (make-book title authors cost n-sold isbn book-list) is a book that has
+;;        title     as the title of the book
+;;        authors   as the list of authors of the book
+;;        cost      as the cost of the book
+;;        n-sold    as the number of copies sold
+;;        book-list as the list of book next to this book
+
+
+; Data definition for ListOfAuthor
+
+;; ListOfAuthor is one of
+;;      -  empty
+;;      - (cons String ListOfAuthor)
+
+
+; Data definition for ListOfBook
+
+;; ListOfBook is one of
+;;      - empty
+;;      - (cons book ListOfBook)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Problem 2: Provide an example of binary search tree containing at least 5 books. 
+;;            Make sure you construct your example so that the items in the tree are 
+;;            ordered according to the binary search tree property, on the ISBN number.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Problem 3: Write the template(s) for the data definition(s) in Problem 1.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; Template for book
+
+(define (fun-for-book abook)
+    (... (book-title                   abook)
+         (fun-for-author (book-authors abook))
+         (book-cost                    abook)
+         (book-n-sold                  abook)
+         (book-isbn                    abook)
+         (fun-for-lob (book-book-list  abook))))
+
+       
+; Template for ListOfAuthor
+
+(define (fun-for-author loa)
+    (cond
+        [(empty? loa) (...)]
+        [(cons?  loa) (... (first loa)
+                           (fun-for-author (rest loa)))]))
+                           
+                           
+; Template for ListOfBook
+
+(define (fun-for-lob lob)
+    (cond
+        [(empty? lob) (...)]
+        [(cons?  lob) (... (fun-for-book (first lob))
+                           (fun-for-lob (rest lob)))]))
+                           
+                           
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Problem 6: Write a function author-of-book? which consumes a binary search tree,
+;;                                                            an ISBN number, 
+;;                                                            and the name of an author. 
+;;            The function returns true if one of the authors of the book with the given ISBN number has the given name. 
+;;            If a book with the given ISBN doesn't exist in the tree, the function should return false. 
+;; Your function should be written efficiently, 
+;; such that it performs as few comparisons as is necessary to find the correct ISBN number in the tree.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; author-of-book? : ListOfBook number string -> boolean
+;; Purpose: return true if one of the authors of the book with the given ISBN is the same as the given name
+;;                 false if the book with the given ISBN doesn't exist / doesn't have the given author
+
+(define (author-of-book? lob isbn author)
+    ())
