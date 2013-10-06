9eb282d4eebd7addf1ef0a3bd9c487ad388f757a
arita1000 <hoangminhminh@gmail.com>
2013-10-06 16:44:11 -0400

    re-write comments

--- ngo-bozdo-hw6.rkt
+++ ngo-bozdo-hw6.rkt
@@ -2,15 +2,16 @@
 ;; Usernames: hmngo     & mbozdo
 ;; ----------------------------------------
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; ########################################## PART 1 ################################################
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ############################################### PART 1 ######################################################
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;  Problem 1: Provide data definitions for a river system. 
-; For each river in the hierarchy, you should record the following information:                      
-;        the name of the river, the pH of the water, the DO in parts per million, 
-;        and a list of the tributaries (rivers) that feed into the river.
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;  Problem 1: Provide data definitions for a river system. 
+;; For each river in the hierarchy, you should record the following information:                      
+;;        the name of the river, the pH of the water, the DO in parts per million, 
+;;        and a list of the tributaries (rivers) that feed into the river.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define-struct river (name pH DO tributaries))
 ;; A river is a (make-river String Number Number ListOfRiver) where
@@ -25,25 +26,27 @@
 ;;       - (cons River ListOfRiver)
 
 
-;  Problem 2: Provide an example of a river system that starts with a single river and               
-;            consists of at least two levels in the hierarchy below that. 
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;  Problem 2: Provide an example of a river system that starts with a single river and               
+;;            consists of at least two levels in the hierarchy below that. 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define River1 
-  (make-river "Missouri" 7 6 
-              (list (make-river "Jefferson" 7.5 9 
-                                (list (make-river "Beaverhead" 9 11  empty) 
-                                      (make-river "Big Hole"   9 6.5 empty)))
-                    (make-river "Sun" 14 12 empty)
-                    (make-river "Yellowstone" 10 7 (list (make-river "Gardner" 5  11 empty) 
-                                                         (make-river "Sheilds" 13 12 empty)
-                                                         (make-river "Boulder" 1  1  empty)))
-                    (make-river "Madison"  1 12 empty)
-                    (make-river "Gallatin" 2 6  empty))))
+			 (make-river "Missouri" 7 6 
+      			        (list (make-river "Jefferson" 7.5 9 
+            			                    (list (make-river "Beaverhead" 9 11  empty) 
+                  			                    (make-river "Big Hole"   9 6.5 empty)))
+                    			(make-river "Sun" 14 12 empty)
+                    			(make-river "Yellowstone" 10 7 (list (make-river "Gardner" 5  11 empty) 
+                        			                                 (make-river "Sheilds" 13 12 empty)
+                              			                           (make-river "Boulder" 1  1  empty)))
+                    			(make-river "Madison"  1 12 empty)
+                  				(make-river "Gallatin" 2 6  empty))))
 
 
-;  Problem 3: Provide the templates for your data definitions.                                      
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;  Problem 3: Provide the templates for your data definitions.                                      
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; fun-for-river: River...-> ...
 
@@ -62,12 +65,13 @@
                          (fun-for-lor (rest alor).....)]))
 
 
-;  Problem 4: Develop a function lower-ph-than that consumes a river system and a number.
-;             The function produces a list of the names of each river in the given system that has 
-; a pH value lower than the given pH.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;  Problem 4: Develop a function lower-ph-than that consumes a river system and a number.
+;;             The function produces a list of the names of each river in the given system that has 
+;; a pH value lower than the given pH.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-
-;; ----------------------------------------- MAIN FUNCTION ----------------------------------------
+;; ---------------------------------------------- MAIN FUNCTION -------------------------------------------------
 ;; Signature: lower-ph-than: river number -> ListOfString
 ;; interp: consumes a river system and a pH value and 
 ;;         produces a list of the names of each river that has a pH lower than the given pH value
@@ -79,7 +83,7 @@
       (lower-ph-than/list (river-tributaries a-river) limit)))
 
 
-;; ---------------------------------------- HELPER FUNCTIONS --------------------------------------
+;; --------------------------------------------- HELPER FUNCTIONS -----------------------------------------------
 ;; Signature: lower-ph-than/list: ListOfRiver number -> ListOfString
 ;; Interp: consumes a list of rivers and a pH values and 
 ;;         produces a list of names of the rivers in the list that have a lower pH value than the given value
@@ -97,17 +101,18 @@
               (list "Buna"))
 
 
-;  Problem 5: Develop a function healthy? that consumes a river system and produces a boolean. 
-;             The function returns true if every river in the system has a pH between 6.5 and 8.5,    
-;             and a DO of at least 6ppm.
- 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;  Problem 5: Develop a function healthy? that consumes a river system and produces a boolean. 
+;;             The function returns true if every river in the system has a pH between 6.5 and 8.5,    
+;;             and a DO of at least 6ppm.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; ------------------------------------------- CONSTANTS ------------------------------------------
+;; ------------------------------------------------ CONSTANTS ---------------------------------------------------
 (define LOWER-BOUND-PH 6.5)
 (define UPPER-BOUND-PH 8.5)
 (define LOWER-BOUND-DO 6)
 
-;; ----------------------------------------- MAIN FUNCTION ----------------------------------------
+;; ---------------------------------------------- MAIN FUNCTION -------------------------------------------------
 ;; Signature: healthy? : river -> boolean
 ;; interp: consumes a river and 
 ;;         produces true if each river in the system has pH between 6.5 and 8.5 and DO at least 6ppm
@@ -120,7 +125,7 @@
        (all-healthy? (river-tributaries a-river))))
     
 
-;; ---------------------------------------- HELPER FUNCTIONS --------------------------------------
+;; --------------------------------------------- HELPER FUNCTIONS -----------------------------------------------
 ;; Signature: all-healthy? : ListOfRiver -> Boolean
 ;; interp: consumes a list of rivers and 
 ;;         produces true if each river in the system has pH between 6.5 and 8.5 and DO at least 6ppm
@@ -176,16 +181,17 @@
               false)
 
 
-;  Problem 6: Develop a function lower-all-ph consumes a river system and produces a river system. 
-;             The river system that is produced is the same as the original, 
-;             except that the pH of all the rivers in the system have been lowered by 0.1.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;  Problem 6: Develop a function lower-all-ph consumes a river system and produces a river system. 
+;;             The river system that is produced is the same as the original, 
+;;             except that the pH of all the rivers in the system have been lowered by 0.1.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-
-;; ------------------------------------------- CONSTANTS ------------------------------------------
+;; ------------------------------------------------ CONSTANTS ---------------------------------------------------
 (define SUBTRAHEND-PH .1)
 
 
-;; ----------------------------------------- MAIN FUNCTION ----------------------------------------
+;; ---------------------------------------------- MAIN FUNCTION -------------------------------------------------
 ;; Signature: lower-all-ph : river -> river
 ;; Interp: consumes a river and 
 ;;         produces another river the same as the first one but with pH lowered by 0.1
@@ -197,7 +203,7 @@
                  (lower-all-ph/list (river-tributaries a-river))))
 
 
-;; ---------------------------------------- HELPER FUNCTIONS --------------------------------------
+;; --------------------------------------------- HELPER FUNCTIONS -----------------------------------------------
 ;; Signature: lower-all-ph/list : ListOfRiver -> ListOfRiver
 ;; Interp: consumes a list of river and
 ;;         produces another list of river the same as the first one but with pH lowered by 0.1
@@ -219,6 +225,7 @@
                                              (make-river "Boulder" 0.9 1 empty)))
         (make-river "Madison" 0.9 12 empty)
         (make-river "Gallatin" 1.9 6 empty))))
+        
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; ########################################## PART 2 ################################################
@@ -246,9 +253,10 @@
 (define BORROWER-LIST-2 (cons BORROWER-2 (cons BORROWER-2 (cons BORROWER-3 empty))))
 
 
-;  Problem 7: Using map and/or filter,     
-;             redefine the function find-by-country that you wrote for Homework 4                            
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;  Problem 7: Using map and/or filter,     
+;;             redefine the function find-by-country that you wrote for Homework 4                            
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; find-by-country :  String ListOfBorrower -> ListOfBorrower
 ;; interp. consumes the name of a country and a list of borrowers and 
@@ -271,8 +279,9 @@
     (filter is-country? alob)))
 
 
-;  Problem 8: Using map and/or filter, define a new function list-all-businesses                            
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;  Problem 8: Using map and/or filter, define a new function list-all-businesses                            
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; list-all-businesses :  ListOfBorrower -> ListOfString
 ;; interp. consumes a list of borrowers and
@@ -293,8 +302,9 @@
   (map borrower-name alob))
 
 
-;  Problem 9: Using map and/or filter, define a new function names-large-loans                              
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;  Problem 9: Using map and/or filter, define a new function names-large-loans                              
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; names-large-loans :  ListOfBorrower Number -> ListOfString
 ;; interp. consumes a list of borrowers and the amount for a loan and
